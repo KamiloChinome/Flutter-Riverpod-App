423653c3260f24b11e80f11f06a453c99db65fb9
Kamilo Chinome <kamiloandresch@gmail.com>
2023-04-25 10:39:14 -0500

    Family Future Provider

--- lib/presentation/providers/future_providers.dart
+++ lib/presentation/providers/future_providers.dart
@@ -5,9 +5,13 @@ final pokemonNameProvider = FutureProvider<String>((ref) async {
   final pokemonId = ref.watch(pokemonIdProvider);
   final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
   return pokemonName;
-
 });
 
 final pokemonIdProvider = StateProvider<int>((ref) {
   return 1;
+});
+
+final pokemonProvider = FutureProvider.family<String, int>((ref, pokemonId) async {
+  final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
+  return pokemonName;
 });
